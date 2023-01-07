60221dd73cf29295c11c92275fb5d765013c6942
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2023-01-07 23:51:16 +0530

    DSA

---
+++ Mathematics/Trailingzeroesinfactorial..cpp
@@ -0,0 +1,13 @@
+int countZeroes(int n){
+    int fact = 1;
+    for(int i = 2; i <= n ; i++){
+        fact = fact*i;
+    int res = 0;
+    while (fact % 10 == 0)      
+    {
+        res++;
+        fact = fact/10;
+    }
+    return res;
+    }
+}
--- Mathematics/factorialofnumber.cpp
+++ Mathematics/factorialofnumber.cpp
@@ -1,5 +1,5 @@
-// Iterative Implementation
-// Time Complexity is Theta(n)
+// Iterative Implementation is better
+// Time Complexity is Theta(n) and space complexity is O(1)
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -11,5 +11,19 @@ for (int i = 2; i < n; i++)
     res = res * i;
     return res;
 }
-
 }
+
+
+// Recursive Solution
+// Assumption >= 0
+// Time Complexity is T(n) = T(n-1) + theta(1)
+// Auxilary space is theta(n)
+
+int facto(int n){
+    if(n == 0){
+        return 1;
+        return n*facto(n-1);
+    }
+    }
+
+
