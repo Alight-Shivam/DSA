c2961099c9d0d9e344a14bfd6904b8864622f74b
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2023-03-28 15:42:37 +0530

    DSA

--- OOPs/tut1.cpp
+++ OOPs/tut1.cpp
@@ -8,11 +8,33 @@ class Hero{
     private:
     int health;
     public:
+    char *name;
     char level;
 
+   Hero(){
+    cout << "Constructor Called" << endl;
+    name = new char[100];
+   }
+
+    // PArameterised Constructor
+    Hero(int health){
+       this -> health = health;
+    }
+    Hero(int health,char level){
+       this -> level = level;
+       this -> health = health;
+    }
+
+
+// Copy Constructor
+Hero(Hero& temp){
+     this -> level = temp.level;
+       this -> health = temp.health;
+}
     void print(){
         cout<<level<< endl;
-
+        cout << "health " << this->health << endl;
+        cout << "level " << this->level << endl;
     }
 
     int getHealth(){
@@ -32,22 +54,36 @@ class Hero{
 // 1 byte memory is allocated to the instance of object created for keeping the track of it or for identifying it
 int main(){
 
+
+
+Hero S(70, 'C');
+S.print();
+ 
+ //Copy Constructor
+Hero R(S);
+R.print();
     //Static Allocation
-    Hero a;
-    a.setHealth(80);
-    a.setlevel('8');
-cout << "level is " << a.level << endl;
-cout << "health is " << a.getHealth() << endl;
+    // Hero a;
+//     Hero ramesh(10);
+// cout << "Address of ramesh" << &ramesh << endl;
+// ramesh.print();
 
-    // Dynamic Allocation
-    Hero *b = new Hero;
-    b->setlevel('A');
-    b->setHealth(70);
-cout << "level is " << (*b).level << endl;
-cout << "health is " << (*b).getHealth() << endl;
+// Hero temp(22, '8');
 
-cout << "level is " << b->level << endl;
-cout << "health is " << b->getHealth() << endl;
+//     a.setHealth(80);
+//     a.setlevel('8');
+// cout << "level is " << a.level << endl;
+// cout << "health is " << a.getHealth() << endl;
+
+//     // Dynamic Allocation
+//     Hero *b = new Hero;
+//     b->setlevel('A');
+//     b->setHealth(70);
+// cout << "level is " << (*b).level << endl;
+// cout << "health is " << (*b).getHealth() << endl;
+
+// cout << "level is " << b->level << endl;
+// cout << "health is " << b->getHealth() << endl;
 //dereferencing the memory 
 
 
