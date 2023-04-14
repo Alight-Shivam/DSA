8015cc9d1c9e980e243dce80793a247e1f378859
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2023-04-14 19:27:41 +0530

    DSA

--- Recursion/output/RecursionOutputPractice.cpp
+++ Recursion/output/RecursionOutputPractice.cpp
@@ -9,13 +9,32 @@ using namespace std;
 //     cout << n <<endl;
 // }
 
+// void fun(int n){
+//     if(n==0)
+//     return;
+//     fun(n-1);
+//     cout<<n<<endl;
+//     fun(n-1);
+// }
+
+// int fun(int n){
+//     if(n==1)
+//     return 0;
+//     else
+//     return 1+fun(n/2); 
+// }
+//  Floor of log base 2 n will be is solution
+
+
+// Recursive function for printing binary of of a number
+
 void fun(int n){
     if(n==0)
     return;
-    fun(n-1);
-    cout<<n<<endl;
-    fun(n-1);
+    fun(n/2);
+    cout << n%2;
 }
+
 int main(){
     fun(3);
     return 0;
