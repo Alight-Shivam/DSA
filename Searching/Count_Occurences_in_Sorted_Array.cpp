6b8d9925e5add00ecb044b5c3c4cdb791153e701
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2023-05-06 19:12:23 +0530

    DSA

---
+++ Searching/Count_Occurences_in_Sorted_Array.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countOcc(int arr[], int n, int x){
+    int first =  firstOcc(arr, n ,x);
+    if(first==-1)
+    return 0;
+    else
+    return(lastOcc(arr,n,x)-first+1);
+}
