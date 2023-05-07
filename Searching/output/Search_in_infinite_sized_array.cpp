29cea897def70eece27be779e01ac7c36ec995bc
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2023-05-08 00:42:59 +0530

    DSA

---
+++ Searching/output/Search_in_infinite_sized_array.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ int search(int arr[], int x){
+    if (arr[0] == x)return 0;
+    int i=1;
+    while(arr[i]<x)
+    i=i*2;
+    if(arr[i]==x)return i;
+
+return binarySearch(arr,x,i/2+1,i)    
+ }
