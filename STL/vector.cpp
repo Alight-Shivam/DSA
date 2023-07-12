3c352cbdb0fe017b24c335409840479586cc8660
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2023-07-12 17:49:08 +0530

    STL revision

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int main(){
     vector<int> v;
+    vector<int> a(5,1);
+
+    vector<int> last(a);
+    cout<<"print last"<<endl;
+    for(int i:last){
+        cout<<i<<" ";
+    }cout<<endl;
+
     v.push_back(1);
     v.push_back(44);
     v.push_back(47);
