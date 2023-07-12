fa87d6d31ad4f0e4a72e73f668e11daca57549c4
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2023-07-12 23:40:51 +0530

    STL revision

--- STL/deque.cpp
+++ STL/deque.cpp
@@ -8,4 +8,21 @@ int main(){
     for(int i:d){
         cout << i << " ";
     }
+// d.pop_front();
+cout<<endl;
+
+
+cout<<"Print First Index Element-> "<<d.at(1)<<endl;
+
+cout<<"front "<<d.front()<<endl;
+cout<<"back "<<d.back()<<endl;
+
+cout<<"Empty or not"<<d.empty()<<endl;
+
+cout<<"Before erase"<<d.size()<<endl;
+d.erase(d.begin(),d.begin()+1);
+cout<<"After erase"<<d.size()<<endl;
+for(int i:d){
+    cout<<i<<" ";
+}
 }
