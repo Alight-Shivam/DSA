9ac7fb8d5fe81a6ae7451b6e8c87d58df7511060
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2023-07-13 01:39:47 +0530

    STL revision

---
+++ STL/Queue.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    queue<string> q;
+    q.push("Shivam");
+    q.push("Middha");
+    q.push("Alight");
+    cout<<"Size before pop"<<q.size()<<endl;
+    cout<<"First Element "<<q.front()<<endl;
+    q.pop();
+    cout<<"First Element "<<q.front()<<endl;
+    cout<<"Size after pop "<<q.size()<<endl;
+}
