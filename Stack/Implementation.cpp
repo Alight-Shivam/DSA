d1900393d3ce6c28cbe0cdb556cff06c5d4cd64a
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2023-07-16 20:47:41 +0530

    DSA in progress

---
+++ Stack/Implementation.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    /*
+    // creation of stack
+    stack <int> s;
+
+    // push operation
+    s.push(2);
+    s.push(3);
+
+    // pop operation
+    s.pop();
+
+    cout << "Printing top element " << s.top() << endl;
+if (s.empty())      
+{
+    cout << "Stack is empty" << endl;
+}
+else{
+    cout << "rehne de bhai bss" << endl; 
+}
+cout << "size of stack is " << s.size() <<endl;
+*/
+
+class Stack{
+    // properties
+
+
+    // functions
+}
+    return 0;
+}
